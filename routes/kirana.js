96b80e0b6f3be76593b5de02776f0f85a4e566dd
Saurabh Dhakate <saurabh.dhakate@loanframe.com>
2021-04-29 11:03:14 +0530

    update

--- routes/kirana.js
+++ routes/kirana.js
@@ -1,10 +1,7 @@
 const express = require('express');
 const mysql = require('mysql');
-const Axios = require('axios')
 const router = express.Router();
-const noop = () => { };
-
-const IMG_API_KEY = '28c9b266952ca9d1a09972ed08c96fb9';
+const noop = _ => { };
 
 const DB_CRED = {
     host: process.env.DB_HOST || 'remotemysql.com',
@@ -13,45 +10,52 @@ const DB_CRED = {
     database: process.env.DB_NAME || 'iFOyzVyv69'
 }
 
-router.get('/store', (req, res) => {
+const query = (dbquery) => {
     let db = mysql.createConnection(DB_CRED)
     db.connect(noop)
     db.on('error', noop)
-    db.query('select * from shops;', (e, d) => {
-        e ? noop : res.json(d)
+    return new Promise((resolve, reject) => {
+        db.query(dbquery, (error, data) => {
+            if (error) {
+                reject(error)
+                db.end(noop)
+            } else {
+                resolve(data)
+                db.end(noop)
+            }
+        })
     })
-    db.end(noop)
+}
+
+router.get('/store', (req, res) => {
+    let dbquery = 'select * from shops;'
+    query(dbquery).then(data => res.json(data)).catch(noop)
 })
 
 router.get('/items/:id', (req, res) => {
-    let db = mysql.createConnection(DB_CRED)
-    db.connect(noop)
-    db.on('error', noop)
-    db.query(`select * from items where shop_id = ${req.params.id};`, (e, d) => {
-        e ? noop : res.json(d)
-    })
-    db.end(noop)
+    let dbquery = `select * from items where shop_id = ${req.params.id};`
+    query(dbquery).then(data => res.json(data)).catch(noop)
 })
 
 router.post('/store', (req, res) => {
-    let db = mysql.createConnection(DB_CRED)
-    db.connect(noop)
-    db.on('error', noop)
-    db.query(`INSERT INTO shops (name, phone, timing, address, image) VALUES ('${req.body.name}', '${req.body.phone}', '${req.body.timing}', '${req.body.address}', '${req.body.image}')`, (e, d) => {
-        e ? console.log(e) : res.json(d)
-    })
-    db.end(noop)
+    let dbquery = `INSERT INTO shops (name, phone, timing, address, image) VALUES ('${req.body.name}', '${req.body.phone}', '${req.body.timing}', '${req.body.address}', '${req.body.image}')`
+    query(dbquery).then(data => res.json(data)).catch(noop)
 })
 
 router.post('/item', (req, res) => {
-    console.log(req.body)
-    let db = mysql.createConnection(DB_CRED)
-    db.connect(noop)
-    db.on('error', noop)
-    db.query(`INSERT INTO items (shop_id, name, price, image) VALUES ('${Number(req.body.shopId)}', '${req.body.name}', '${req.body.price}', '${req.body.image}')`, (e, d) => {
-        e ? console.log(e) : res.json(d)
-    })
-    db.end(noop)
+    let dbquery = `INSERT INTO items (shop_id, name, price, image) VALUES ('${Number(req.body.shopId)}', '${req.body.name}', '${req.body.price}', '${req.body.image}')`
+    query(dbquery).then(data => res.json(data)).catch(noop)
+})
+
+router.get('/removeshop/:id', (req, res) => {
+    let dbquery1 = `DELETE FROM shops WHERE id = ${req.params.id};`
+    let dbquery2 = `DELETE FROM items WHERE shop_id = ${req.params.id};`
+    query(dbquery1).then(_ => query(dbquery2)).then(_ => res.json(_)).catch(_ =>res.json({}))
+})
+
+router.get('/removeitem/:id', (req, res) => {
+    let dbquery = `DELETE FROM items WHERE id = ${req.params.id};`
+    query(dbquery).then(data => res.json(data)).catch(noop)
 })
 
 module.exports = router;
