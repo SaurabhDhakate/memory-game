f788dcf747e3e8bbb4fdb6d1d17b456cae8b64d9
Saurabh Dhakate <saurabh.dhakate@loanframe.com>
2021-05-01 17:46:17 +0530

    LOGOUT OTP EMAIL

--- routes/kirana.js
+++ routes/kirana.js
@@ -116,7 +116,7 @@ router.get('/otp', (req, res) => {
 
 router.get('/logout', (req, res) => {
     let email = req.query.email
-    let otp = new Date()
+    let otp = new Date().getTime()
     query(`UPDATE k_users SET otp = '${otp}' WHERE email = '${email}';`)
         .then(()=>{res.status(200); res.end()})
         .catch((e)=>{console.log(e);res.status(404); res.end()})
